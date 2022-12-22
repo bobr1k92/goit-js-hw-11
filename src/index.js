c2383e483a70b0fb29842054b084d3113391ad28
Denis Mazurov <19bobrik92@gmail.com>
2022-12-23 00:04:43 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,31 +1,70 @@
-import NewsApiSevice from './js/api-service';
-
-import axios from 'axios';
-import Notiflix from 'notiflix';
+import PictureApiSevice from './js/api-service';
+import {Notify} from 'notiflix';
 import simpleLightbox from 'simplelightbox';
 
 
 
 const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    articlesContainer: document.querySelector('.js-articles-container'),
-    loadMoreBtn: document.querySelector('[data-action="load-more"]')
+    searchForm: document.querySelector('#search-form'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    galleryContainer: document.querySelector('.gallery')
 }
 
-const newsApiService = new NewsApiSevice();
+const pictureAPI = new PictureApiSevice();
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 
-function onSearch(e) {
+async function onSearch(e) {
     e.preventDefault();
 
-    newsApiService.query = e.currentTarget.elements.query.value;
-    newsApiService.resetPage();
-    newsApiService.fetchArticles().then(articles => console.log(articles));
+    pictureAPI.query = e.currentTarget.elements.searchQuery.value.trim();
+    if (pictureAPI.query === '') {
+      Notify.warning('Введіть що небудь');
+    }
+    pictureAPI.resetPage();
+  try {
+    const {hits, totalHits} = await pictureAPI.fetchAPI();
+    renderPictures(hits, totalHits)
+  } catch (error) {
+    Notify.failure("Винекла помилка");
+  }
+
+async function onLoadMore() {
+  const {hits, totalHits} = await pictureAPI.fetchAPI();
+ }
 }
 
-function onLoadMore() {
-    newsApiService.fetchArticles().then(articles => console.log(articles));
+
+function renderPictures(hits) {
+const images = hits.map(({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+}) => {
+    return `<div class="photo-card">
+    <img src="${ webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: </b>${likes}
+      </p>
+      <p class="info-item">
+        <b>Views: </b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments: </b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads: </b>${downloads}
+      </p>
+    </div>
+  </div>`
+}).join('');
+
+refs.galleryContainer.insertAdjacentHTML('beforeend', images);
 }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,26 +1,27 @@
-export default class NewsApiService {
+import axios from "axios";
+
+const API_KEY = '32277249-d7139627b7e0a08a8752d2e83';
+axios.defaults.baseURL = 'https://pixabay.com/api/'
+
+export default class PictureApiSevice {
     constructor() {
     this.searchQuery = '';
     this.page = 1;
     }
 
-    fetchArticles() {
-        console.log(this);
-        const options = {
-            headers: {
-                Authorization: 'b5f9a10038a94cda93723e0a1eeb5c6e',
-            },
-        }
-        const url =
-             `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&${this.page}`;
-             
-     return fetch(url, options)
-    .then(res => res.json())
-    .then(data => {
-        this.incrementPage();
-
-        return data.articles;
-      });
+    async fetchAPI() {
+        const options = new URLSearchParams({
+            key: API_KEY,
+            q: this.searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            page: this.page,
+            per_page: 40,
+        });
+        const { data } = await axios(`?${options}`);
+        console.log(data);
+        return data;
     }
 
     incrementPage() {
